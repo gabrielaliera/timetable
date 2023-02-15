4a6b01f8cdc5863ab00578be8acaf229338547ef
gabrielaliera <gliera2990@gmail.com>
2023-02-15 13:06:04 -0800

    Add H1 and H2 to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ const App = () => {
 
   return (
     <div className="App">
-
-      
+      <h1>Itinerary for 7 Days in Tokyo 🏙</h1>
+      <h2>Welcome to Tokyo, NAME! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+      <Calendar />
     </div>
   )
 }
